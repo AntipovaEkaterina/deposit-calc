05be186855c9207bf5d53dfdcb25333a7e83fc1f
NightCatrin <antipovaekaterina20475@gmail.com>
2017-03-13 00:06:27 +0700

    Implementing the validation functioin

--- main.c
+++ main.c
@@ -1,11 +1,23 @@
 #include <stdio.h> 
 
+int check(int sum, int time)
+{
+	if ((sum >= 10000) && (time <= 365) && (time >= 0))
+		return 1;
+	return 0;
+}
+
 int main()
 {
 	int sum, time;
+	do 
+	{
 		printf("\nInput deposit amount:");
 		scanf("%d", &sum);
 		printf("Input amount of days:");
 		scanf("%d", &time);
-return (0);
+	} 
+	while (check(sum, time)==0);
+
+return 0;
 }
