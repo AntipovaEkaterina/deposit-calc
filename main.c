a9a5a6c19ee88e22940a19563f93e411b12694f0
NightCatrin <antipovaekaterina20475@gmail.com>
2017-03-13 00:17:10 +0700

    Deposit Calculation

--- main.c
+++ main.c
@@ -7,6 +7,25 @@ int check(int sum, int time)
 	return 0;
 }
 
+float calc(int sum, int time)
+{
+	if (time <= 30)
+		return (float) sum * 0.9;
+	if ((time > 30) && (time <= 120) && (sum <= 100000))
+		return (float) sum * 1.02;
+	if ((time > 30) && (time <= 120) && (sum > 100000))
+		return (float) sum * 1.03;
+	if ((time > 121) && (time <= 240) && (sum <= 100000))
+		return (float) sum * 1.06;
+	if ((time > 121) && (time <= 240) && (sum > 100000))
+		return (float) sum * 1.08;
+	if ((time > 241) && (time <= 365) && (sum <= 100000))
+		return (float) sum * 1.12;
+	if ((time > 241) && (time <= 365) && (sum > 100000))
+		return (float) sum * 1.15;
+	return 0;
+}
+
 int main()
 {
 	int sum, time;
@@ -18,6 +37,7 @@ int main()
 		scanf("%d", &time);
 	} 
 	while (check(sum, time)==0);
+	printf("%.2f\n", calc(sum, time));
 
 return 0;
 }
