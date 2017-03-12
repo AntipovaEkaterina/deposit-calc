2e0d3801069c47f22a2b52edba7f4a5748ae6f31
NightCatrin <antipovaekaterina20475@gmail.com>
2017-03-12 23:45:25 +0700

    Data input

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+
+int main()
+{
+	int sum, time;
+		printf("\nInput deposit amount:");
+		scanf("%d", &sum);
+		printf("Input amount of days:");
+		scanf("%d", &time);
+return (0);
+}
