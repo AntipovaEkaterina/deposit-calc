d811ed1b9e0877c70bd54eb5b47a64e84d1532e3
NightCatrin <antipovaekaterina20475@gmail.com>
2017-04-28 23:53:22 +0700

    tets2

--- test/validation_test.c
+++ test/validation_test.c
@@ -1,10 +1,22 @@
 #include <deposit.h>
 #include <ctest.h>
 
-CTEST(validation_test, test1)
+CTEST(validation_test, yes)
 {
-	int time = 199;
-	int sum = 10002;
+	int time = 13;
+	int sum = 10032;
+	
+	int result = check(sum, time);
+	ASSERT_EQUAL(result, 1);
+}
+
+#include <deposit.h>
+#include <ctest.h>
+
+CTEST(validation_test, no)
+{
+	int time = 465;
+	int sum = 124;
 	
 	int result = check(sum, time);
 	ASSERT_EQUAL(result, 0);
