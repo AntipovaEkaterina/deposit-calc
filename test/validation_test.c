70037a5f36a9d5345705f11865b685cfcbd0c5d8
Антипова Екатерина Петровна <iv621s01@awp08.csc.local>
2017-04-29 12:18:22 +0700

    Validation test two

--- test/validation_test.c
+++ test/validation_test.c
@@ -1,7 +1,7 @@
 #include <deposit.h>
 #include <ctest.h>
 
-CTEST(validation_test, yes)
+CTEST(validation_test, yes_yes)
 {
 	int time = 13;
 	int sum = 10032;
@@ -10,12 +10,27 @@ CTEST(validation_test, yes)
 	ASSERT_EQUAL(result, 1);
 }
 
-#include <deposit.h>
-#include <ctest.h>
-
-CTEST(validation_test, no)
+CTEST(validation_test, no_no)
 {
 	int time = 465;
+	int sum = 124;
+	
+	int result = check(sum, time);
+	ASSERT_EQUAL(result, 0);
+}
+
+CTEST(validation_test, no_yes)
+{
+	int time = 465;
+	int sum = 12004;
+	
+	int result = check(sum, time);
+	ASSERT_EQUAL(result, 0);
+}
+
+CTEST(validation_test, yes_no)
+{
+	int time = 15;
 	int sum = 124;
 	
 	int result = check(sum, time);
